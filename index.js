d78f25a3c31dea03deda63d8a9f3529a95af3412
Dane Williamson <danewilliamson@Danes-MacBook-Pro.local>
2018-05-25 02:13:41 -0400

    Updated index.js file to enable server to run in mac OS environments

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const BODYPARSER = require("body-parser")
 var endpoints = require("./endpoints/endpoints.js")
 //
 var webServer = EXPRESS();
-var port = 80;
+var port = 1024; // non-privileged users cannot listen on sock ports below 1024
 
 webServer.use(BODYPARSER.json())
 webServer.use(BODYPARSER.urlencoded({extended:true}))
 
 webServer.use(function(req, res, next){
   console.log(req.url + " " + req.method +" | "+JSON.stringify(req.body));
-  FILESYSTEM.appendFile(__dirname+'\\Server.log',req.ip + " " + new Date() + " " + req.url + " " + req.method +" "+JSON.stringify(req.body) + " ||| " + JSON.stringify(req.query)+"\"\n",{flag:"a"},()=>{});
+  FILESYSTEM.appendFile(__dirname+'\\Server.log',req.ip + " " + new Date() + " " + req.url + " " + req.method +" "+JSON.stringify(req.body) + " ||| " + JSON.stringify(req.query)+"\"\n",{flag:"a"}, function(err)
+  {
+    if(err) console.log(err);
+  } );
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, SEARCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -25,11 +28,13 @@ webServer.use(function(req, res, next){
       res.end();
     }
   else
+  {
     next();
+  }
 });
 
 webServer.use(endpoints)
 webServer.use(EXPRESS.static(__dirname +"\\webpages"))
 webServer.listen(port, function(){
   console.log("webserver running on port " + port);
-})
+});
