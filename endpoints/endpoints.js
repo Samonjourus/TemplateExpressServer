1277a5a68837779b605294b81f9a1631731ec82f
benjamin <benjaminstandfield@gmail.com>
2023-09-30 01:58:07 -0400

    add template documentation using swagger

--- endpoints/endpoints.js
+++ endpoints/endpoints.js
@@ -1,7 +1,32 @@
 const EXPRESS = require("express")
 
 router = EXPRESS.Router();
+/**
+ * tags:
+ *  name: Utility
+ *  description: Designed for debugging/testing purposes.
+ */
 
+/**
+ * @swagger
+ * /api/status:
+ *  get:
+ *    summary: Returns a sucess JSON in the event of a sucessful connection.
+ *    tags: [Utility]
+ *    responses:
+ *      200:
+ *        description: Sucessfully connected to the API server.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: string
+ *                  description: Always set to 'good'.
+ *              example:
+ *                status:good
+ */
 router.get("/api/status", function(req, res){
   res.setHeader('content-type', 'application/json');
   res.end(JSON.stringify({"status":"good"}))
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
+// npm packages
+var swaggerJsdoc = require("swagger-jsdoc")
+var swaggerUi = require("swagger-ui-express");
+var fileSystem = require("fs")
+
+// local imports
 var webServer = require("./app.js")
 
+// parameters
 var port = 8080;
+
+// read package.json fields for populating swagger documentation info
+let packageJSON = JSON.parse(fileSystem.readFileSync("./package.json"))
+
+// swagger documentation parameters
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Express API documentation made with Swagger",
+      version: packageJSON["version"],
+      description:
+        packageJSON["description"],
+      license: {
+        name: packageJSON["license"]
+      },
+      contact: {
+        name: packageJSON["author"],
+        url: "https://www.github.com/samonjourus",
+        email: "benjaminstandfield@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:" + port.toString(),
+      },
+    ],
+  },
+  apis: ["./endpoints/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+webServer.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 webServer.listen(port, function(){
   console.log("webserver running on port " + port);
